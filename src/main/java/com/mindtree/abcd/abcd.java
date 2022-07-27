2449c4ae50652b15ae9ddfc8c3bf2f448cf413e3
Sanjog <M1089110@mindtree.com>
2022-07-28 01:33:31 +0530

    just for checking

---
+++ src/main/java/com/mindtree/abcd/abcd.java
@@ -0,0 +1,22 @@
+package com.mindtree.abcd;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class abcd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		driver.get("https://www.moneycontrol.com/");
+		
+
+	}
+
+}
